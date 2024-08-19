4bad36e783fcb1f0a8c9311367a137ee38db8397
KRISNABADDE <krisnabadde@gamil.com>
2024-08-19 20:17:51 +0530

    Zip file extraction feature added

---
+++ src/dataset.py
@@ -0,0 +1,24 @@
+import typer
+import zipfile
+import pandas as pd
+from loguru import logger
+from tqdm import tqdm
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+from src.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+
+app = typer.Typer()
+
+def extract_files(RAW_DATA_DIR: Path, file_name: str)-> None:
+    
+    zip_file_path = RAW_DATA_DIR / file_name
+    
+    extract_dir = RAW_DATA_DIR / "extracted"
+    
+    extract_dir.mkdir(parents=True, exist_ok=True)
+    
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_dir)
+
+    logger.success("Unzip datafile complete.")
+    
